948deaeb28bd59e57f0daa530bcd76e7bf7f3aaf
ktht <karlehataht@gmail.com>
2018-12-16 21:57:33 +0200

    Add the possibility of creating subdirectories in jet_ntuple_filler

--- JetAnalyzers/bin/jet_ntuple_filler.cc
+++ JetAnalyzers/bin/jet_ntuple_filler.cc
@@ -95,6 +95,8 @@ int main(int argc, char* argv[])
   std::string src_rho = cfg_jet_ntuple_filler.getParameter<std::string>("src_rho");
   std::string src_weight = cfg_jet_ntuple_filler.getParameter<std::string>("src_weight");
   std::string src_pThat = cfg_jet_ntuple_filler.getParameter<std::string>("src_pThat");
+  std::string src_pudensity = cfg_jet_ntuple_filler.getParameter<std::string>("src_pudensity");
+  std::string src_gpudensity = cfg_jet_ntuple_filler.getParameter<std::string>("src_gpudensity");
   
   double dR_match = cfg_jet_ntuple_filler.getParameter<double>("dR_match");
 
@@ -117,7 +119,7 @@ int main(int argc, char* argv[])
     }
     if ( !(jetCorrParams.size() >= 1) ) 
       throw cms::Exception("jet_ntuple_filler")
-	<< "Invalid Configuration parameter 'jetCorrectionLevels' = " << jetCorrectionLevels << " !!\n";
+        << "Invalid Configuration parameter 'jetCorrectionLevels' = " << jetCorrectionLevels << " !!\n";
     jetCorrector = new FactorizedJetCorrector(jetCorrParams);
   }
 
@@ -137,7 +139,7 @@ int main(int argc, char* argv[])
 
   EventInfoReader* evtInfoReader = new EventInfoReader(
     src_numPU, src_numPU_true, src_numVertices, src_vertexZ, src_rho, 
-    src_weight, src_pThat);
+    src_weight, src_pThat, src_pudensity, src_gpudensity);
   inputTree->registerReader(evtInfoReader);
 
   RecoJetReader* recJetReader = new RecoJetReader(src_recJets);
@@ -149,7 +151,30 @@ int main(int argc, char* argv[])
   JetCollectionGenMatcher jetGenMatcher;
 
   std::string outputTreeName = cfg_jet_ntuple_filler.getParameter<std::string>("outputTreeName");
-  TTree* outputTree = fs.make<TTree>(outputTreeName.data(), outputTreeName.data());
+  TDirectory * dir = fs.getBareDirectory();
+  if(outputTreeName.find('/') != std::string::npos)
+  {
+    std::stringstream ss(outputTreeName);
+    std::string segment;
+    std::vector<std::string> seglist;
+    while(std::getline(ss, segment, '/'))
+    {
+      if(! segment.empty())
+      {
+        seglist.push_back(segment);
+      }
+    }
+    outputTreeName = seglist.back();
+    seglist.pop_back();
+    for(const std::string & subdirName: seglist)
+    {
+      TDirectory * subdir = dir->mkdir(subdirName.c_str());
+      subdir->cd();
+      dir = subdir;
+    }
+  }
+  dir->cd();
+  TTree* outputTree = new TTree(outputTreeName.data(), outputTreeName.data());
   std::bitset<8> outputTree_flags = cfg_jet_ntuple_filler.getParameter<unsigned>("outputTree_flags");
   JRAEvent* outputTree_event = new JRAEvent(outputTree, outputTree_flags);
 
@@ -179,6 +204,10 @@ int main(int argc, char* argv[])
     outputTree_event->npus->push_back(evtInfo.numPU());
     outputTree_event->tnpus->push_back(evtInfo.numPU_true());
     outputTree_event->bxns->push_back(0);
+    outputTree_event->sumpt_lowpt->push_back(0.);
+    outputTree_event->sumpt_highpt->push_back(0.);
+    outputTree_event->ntrks_lowpt->push_back(0);
+    outputTree_event->ntrks_highpt->push_back(0);
     outputTree_event->rho = evtInfo.rho();
     outputTree_event->pthat = evtInfo.pThat();
     outputTree_event->weight = evtInfo.weight();
@@ -187,42 +216,44 @@ int main(int argc, char* argv[])
     outputTree_event->run = evtInfo.run();
     outputTree_event->lumi = evtInfo.lumi();
     outputTree_event->evt = evtInfo.event();
+    outputTree_event->pudensity = evtInfo.pudensity();
+    outputTree_event->gpudensity = evtInfo.gpudensity();
     outputTree_event->refdzvtx->push_back(0);
 
     int numRecJets_selected = 0;
     for ( std::vector<RecoJet>::const_iterator recJet = recJets.begin();
-	  recJet != recJets.end(); ++recJet ) {
+          recJet != recJets.end(); ++recJet ) {
       const GenJet* genJet = recJet->genJet();
       if ( !genJet ) continue;
 
       double sf = 1. - recJet->rawFactor();
       if ( sf <= 1.e-3 ) {
-	std::cout << "Warning: raw jet energy = " << sf*recJet->p4().energy() 
-		  << " --> skipping jet with zero or negative energy !!" << std::endl;
-	continue;
+        std::cout << "Warning: raw jet energy = " << sf*recJet->p4().energy()
+                  << " --> skipping jet with zero or negative energy !!" << std::endl;
+        continue;
       }
       Jet::LorentzVector recJetP4_uncorr(sf*recJet->pt(), recJet->eta(), recJet->phi(), sf*recJet->mass());
       assert(std::fabs(recJetP4_uncorr.eta() - recJet->eta()) < 1.e-2);
       assert(std::fabs(deltaPhi(recJetP4_uncorr.phi(), recJet->phi())) < 1.e-2);
 
       if ( isDEBUG ) {
-	std::cout << "reconstructed jet #" << numRecJets_selected << ":" << std::endl;
-	std::cout << (*recJet);
-	std::cout << std::endl;
+        std::cout << "reconstructed jet #" << numRecJets_selected << ":" << std::endl;
+        std::cout << (*recJet);
+        std::cout << std::endl;
       }
 
       double jec = 1.;
       if ( jetCorrector ) {
-	jetCorrector->setJetEta(recJetP4_uncorr.eta());
-	jetCorrector->setJetPt(recJetP4_uncorr.pt());
-	jetCorrector->setJetA(recJet->area());
-	jetCorrector->setRho(evtInfo.rho());
-	jec = jetCorrector->getCorrection();
-	if ( isDEBUG ) {
-	  std::cout << "ratio of old/new JEC scale factor = " << (recJet->pt()/recJetP4_uncorr.pt())/jec << std::endl;
-	  std::cout << std::endl;
-	}
-	recJetP4_uncorr = Jet::LorentzVector(sf*recJet->pt()*jec, recJet->eta(), recJet->phi(), sf*recJet->mass()*jec);
+        jetCorrector->setJetEta(recJetP4_uncorr.eta());
+        jetCorrector->setJetPt(recJetP4_uncorr.pt());
+        jetCorrector->setJetA(recJet->area());
+        jetCorrector->setRho(evtInfo.rho());
+        jec = jetCorrector->getCorrection();
+        if ( isDEBUG ) {
+          std::cout << "ratio of old/new JEC scale factor = " << (recJet->pt()/recJetP4_uncorr.pt())/jec << std::endl;
+          std::cout << std::endl;
+        }
+        recJetP4_uncorr = Jet::LorentzVector(sf*recJet->pt()*jec, recJet->eta(), recJet->phi(), sf*recJet->mass()*jec);
       }
 
       outputTree_event->refrank->push_back(numRecJets_selected);
@@ -249,7 +280,7 @@ int main(int argc, char* argv[])
       fillBranch(outputTree_event->jtnhf, recJet->neHEF());
       fillBranch(outputTree_event->jtnef, recJet->neEmEF());
       fillBranch(outputTree_event->jtcef, recJet->chEmEF());
-      fillBranch(outputTree_event->jtmuf, 0.); // value not available in nanoAOD 
+      fillBranch(outputTree_event->jtmuf, recJet->muEF());
       fillBranch(outputTree_event->jthfhf, 0.); // value not available in nanoAOD
       fillBranch(outputTree_event->jthfef, 0.); // value not available in nanoAOD
 
