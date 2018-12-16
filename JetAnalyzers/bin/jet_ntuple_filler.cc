b9ad42cecc710177c1cc95024bbadd45c84491d4
ktht <karlehataht@gmail.com>
2018-12-16 23:54:19 +0200

    Run jet_ntuple_filler using AK8Puppi jet collection

--- JetAnalyzers/bin/jet_ntuple_filler.cc
+++ JetAnalyzers/bin/jet_ntuple_filler.cc
@@ -85,6 +85,7 @@ int main(int argc, char* argv[])
   edm::ParameterSet cfg_jet_ntuple_filler = cfg.getParameter<edm::ParameterSet>("jet_ntuple_filler");
 
   std::string inputTreeName = cfg_jet_ntuple_filler.getParameter<std::string>("inputTreeName");
+  bool run_ak4pfchs = cfg_jet_ntuple_filler.getParameter<bool>("run_ak4pfchs");
 
   std::string src_recJets = cfg_jet_ntuple_filler.getParameter<std::string>("src_recJets");
   std::string src_genJets = cfg_jet_ntuple_filler.getParameter<std::string>("src_genJets");
@@ -140,7 +141,7 @@ int main(int argc, char* argv[])
     src_weight, src_pThat);
   inputTree->registerReader(evtInfoReader);
 
-  RecoJetReader* recJetReader = new RecoJetReader(src_recJets);
+  RecoJetReader* recJetReader = new RecoJetReader(src_recJets, run_ak4pfchs);
   inputTree->registerReader(recJetReader);
 
   GenJetReader* genJetReader = new GenJetReader(src_genJets);
